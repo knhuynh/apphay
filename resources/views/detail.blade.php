b67885b9861caadd6320f5a2d5a5fcfe2b7fb2bb
Kim Ngoc Huynh <knhuynhtctv@gmail.com>
2016-12-20 23:38:06 +0700

    [+] First upload

---
+++ resources/views/detail.blade.php
@@ -0,0 +1,108 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="assets/favicon.ico">
+
+    <title>AppHay.com</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/carousel.css" rel="stylesheet">
+
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">AppHay</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right">
+            <div class="form-group">
+              <input type="text" placeholder="Tìm ứng dụng ..." class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Search</button>
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="">
+      <div class="container wrapper">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-8 player">Playing app</div>
+            <div class="col-md-4 clear-padding-right">
+              <div class="col-md-12 app-relate">Apps relate</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="col-md-12 info-comment">
+        <div class="row">
+          <div class="col-md-8 info">Infomation</div>
+        </div>
+        <div class="row">
+          <div class="col-md-8 comment">Comments</div>
+        </div>
+      </div>
+      <div style="clear:both"></div>
+
+      <hr>
+
+      <footer>
+        <ul>
+          <li><a>Về App Hay</a></li>
+          <li><a>Điều khoản sử dụng</a></li>
+          <li><a>&copy; Copyright by Apphay.com</a></li>
+        </ul>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="assets/favicon.ico">
+
+    <title>AppHay.com</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/carousel.css" rel="stylesheet">
+
+    <link href="assets/css/style.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="assets/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">AppHay</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <form class="navbar-form navbar-right">
+            <div class="form-group">
+              <input type="text" placeholder="Tìm ứng dụng ..." class="form-control">
+            </div>
+            <button type="submit" class="btn btn-success">Search</button>
+          </form>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="">
+      <div class="container">
+
+        <!-- Carousel
+        ================================================== -->
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+          <!-- Indicators -->
+          <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+            <div class="item active">
+              <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
+              <div class="container">
+                <div class="carousel-caption">
+                  <h1>Example headline.</h1>
+                  <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+                  <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+              <div class="container">
+                <div class="carousel-caption">
+                  <h1>Another example headline.</h1>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                </div>
+              </div>
+            </div>
+            <div class="item">
+              <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+              <div class="container">
+                <div class="carousel-caption">
+                  <h1>One more for good measure.</h1>
+                  <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                  <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div><!-- /.carousel -->
+
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="col-md-3 app-item">
+          <a><img src="http://www.appnhe.com/data/images/thumbnail/1113_normal.jpg" class="img-responsive" /></a>
+          <div class="col-md-12 title-play-date">
+            <a><h2>Bao nhiêu người đã cảm nắng bạn trong năm 2016?</h2></a>
+            <ul>
+              <li><span class="glyphicon glyphicon-expand"></span>1,366</li>
+              <li><span class="glyphicon glyphicon-calendar"></span>3 days ago</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3 app-item">
+          <a><img src="http://www.appnhe.com/data/images/thumbnail/47_normal.jpg" class="img-responsive" /></a>
+          <div class="col-md-12 title-play-date">
+            <a><h2>Chọn ĐÚNG hoặc SAI trong 3 nốt nhạc!</h2></a>
+            <ul>
+              <li><span class="glyphicon glyphicon-expand"></span>1,366</li>
+              <li><span class="glyphicon glyphicon-calendar"></span>3 days ago</li>
+            </ul>
+          </div>
+       </div>
+        <div class="col-md-3 app-item">
+          <a><img src="http://www.appnhe.com/data/images/thumbnail/1109_normal.jpg" class="img-responsive" /></a>
+          <div class="col-md-12 title-play-date">
+            <a><h2>Bức ảnh nhiều like nhất năm 2016 của bạn</h2></a>
+            <ul>
+              <li><span class="glyphicon glyphicon-expand"></span>1,366</li>
+              <li><span class="glyphicon glyphicon-calendar"></span>3 days ago</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3 app-item">
+          <a><img src="http://www.appnhe.com/data/images/thumbnail/1099_normal.jpg" class="img-responsive" /></a>
+          <div class="col-md-12 title-play-date">
+            <a><h2>Ai luôn lấy bạn ra làm gương?</h2></a>
+            <ul>
+              <li><span class="glyphicon glyphicon-expand"></span>1,366</li>
+              <li><span class="glyphicon glyphicon-calendar"></span>3 days ago</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3 app-item">
+          <a><img src="http://www.appnhe.com/data/images/thumbnail/1113_normal.jpg" class="img-responsive" /></a>
+          <div class="col-md-12 title-play-date">
+            <a><h2>Bao nhiêu người đã cảm nắng bạn trong năm 2016?</h2></a>
+            <ul>
+              <li><span class="glyphicon glyphicon-expand"></span>1,366</li>
+              <li><span class="glyphicon glyphicon-calendar"></span>3 days ago</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3 app-item">
+          <a><img src="http://www.appnhe.com/data/images/thumbnail/47_normal.jpg" class="img-responsive" /></a>
+          <div class="col-md-12 title-play-date">
+            <a><h2>Chọn ĐÚNG hoặc SAI trong 3 nốt nhạc!</h2></a>
+            <ul>
+              <li><span class="glyphicon glyphicon-expand"></span>1,366</li>
+              <li><span class="glyphicon glyphicon-calendar"></span>3 days ago</li>
+            </ul>
+          </div>
+       </div>
+        <div class="col-md-3 app-item">
+          <a><img src="http://www.appnhe.com/data/images/thumbnail/1109_normal.jpg" class="img-responsive" /></a>
+          <div class="col-md-12 title-play-date">
+            <a><h2>Bức ảnh nhiều like nhất năm 2016 của bạn</h2></a>
+            <ul>
+              <li><span class="glyphicon glyphicon-expand"></span>1,366</li>
+              <li><span class="glyphicon glyphicon-calendar"></span>3 days ago</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-3 app-item">
+          <a><img src="http://www.appnhe.com/data/images/thumbnail/1099_normal.jpg" class="img-responsive" /></a>
+          <div class="col-md-12 title-play-date">
+            <a><h2>Ai luôn lấy bạn ra làm gương?</h2></a>
+            <ul>
+              <li><span class="glyphicon glyphicon-expand"></span>1,366</li>
+              <li><span class="glyphicon glyphicon-calendar"></span>3 days ago</li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-12 text-center next-prev">
+          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-arrow-left"></span> Quay lại</button>
+          <button type="button" class="btn btn-default">Tiếp theo <span class="glyphicon glyphicon-arrow-right"></span></button>
+        </div>
+      </div>
+
+      <hr>
+
+      <footer>
+        <ul>
+          <li><a>Về App Hay</a></li>
+          <li><a>Điều khoản sử dụng</a></li>
+          <li><a>&copy; Copyright by Apphay.com</a></li>
+        </ul>
+      </footer>
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
